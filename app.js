95c79f03cd345a9b72c93de566269c4b05f3df0e
Solirss <youngminkim2002@gmail.com>
2024-11-28 16:15:24 +0900

    Updated the database route to joy project

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes.js');
+const cookieParser = require('cookie-parser');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware.js');
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+//We need to use express.json when we want to take in json from the user side 
+app.use(express.json());
+app.use(cookieParser());
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = 'mongodb+srv://Joy-bread:13888147114@cluster0.aza85.mongodb.net/Joy-bread';
+mongoose.connect(dbURI)
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// routes
+app.get('*', checkUser);
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', requireAuth,(req, res) => res.render('smoothies'));
+app.use(authRoutes);
